3b737fd276766ab55fdf8f88ebab8c1da753da04
Franco Natanael Tomaino <tomainofranco5@gmail.com>
2024-08-23 21:12:29 -0300

    proyect structure and basic nav

---
+++ front/app/components/NavBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar(){
+    return <nav>
+        <ul>
+            <li>Explore</li>
+            <li>My repos</li>
+            <li>Bookmarks</li>
+            <li>Settings</li>
+        </ul>
+    </nav>
+}
---
+++ front/app/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "./components/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar/>
+      <h1 className="w-full text-center font-bold my-5">Find GitHub repos by interests</h1>
+    </div>
+  );
+}
